ee282fdf57a24902391341baaefb284c1a89cb2f
Daniel Siepmann <d.siepmann@web-vision.de>
2016-02-09 17:39:00 +0100

    [CLEANUP] Refactor
    * Clean up and structure code
    
    Related: DSI-68

--- Classes/Controller/PagesController.php
+++ Classes/Controller/PagesController.php
@@ -15,13 +15,11 @@ namespace WebVision\WvTranslation\Controller;
  */
 
 use TYPO3\CMS\Backend\View\BackendTemplateView;
-use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
- * TODO: Refactor, fix query's to respect enable fields like deleted.
- * Move most code to separate classes.
+ * Controller to provide translation information for pages.
  *
  * @author Daniel Siepmann <d.siepmann@web-vision.de>
  */
@@ -34,92 +32,43 @@ class PagesController extends ActionController
      */
     protected $defaultViewObjectName = BackendTemplateView::class;
 
+    /**
+     * @inject
+     * @var WebVision\WvTranslation\Domain\Repository\LocalizationRepository
+     */
+    protected $localizationRepository;
+
+    /**
+     * Initialize all actions.
+     *
+     * Fetch current page uid.
+     */
     protected function initializeAction()
     {
-        $this->initializeCurrentPage();
-    }
-
-    public function indexAction()
-    {
-        $this->view->assignMultiple([
-            'pages' => $this->getPages($this->settings['currentPage']),
-            'languages' => $this->getLanguages(),
-        ]);
+        $this->settings['currentPageUid'] = 0;
+        $currentPageUid = GeneralUtility::_GET('id');
+        if ($currentPageUid !== null) {
+            $this->settings['currentPageUid'] = (int) $currentPageUid;
+        }
     }
 
     /**
-     * Initialize the current page, from page tree, and persist to settings.
+     * Deliver an index of pages, with there current localization stage.
+     *
+     * The list can is recursive and parent can be set with get var "id". 0 is
+     * default.
      *
      * @return void
      */
-    protected function initializeCurrentPage()
+    public function indexAction()
     {
-        $currentPage = GeneralUtility::_GET('id');
-        $pageRecord = [
-            'uid' => 0,
-            'title' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'],
-        ];
-        if ($currentPage === null) {
-            $currentPage = 0;
-        } else {
-            $pageRecord = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow(
-                '*',
-                'pages',
-                'uid = ' . (int) $currentPage
-            );
-        }
-
-        $page = GeneralUtility::makeInstance(\TYPO3\CMS\Backend\Tree\Pagetree\PagetreeNode::class);
-        $page->setId($currentPage);
-        $page->setRecord($pageRecord);
-        $this->addLanguageOverlay($page);
-        $this->settings['currentPage'] = $page;
+        $currentPage = $this->localizationRepository->findPageByUid($this->settings['currentPageUid']);
+        $this->view->assignMultiple([
+            'currentPage' => $currentPage,
+            'pages' => $this->localizationRepository->findPagesByParentPage($currentPage),
+            'languages' => $this->localizationRepository->findAllSystemLanguages(),
+        ]);
     }
 
-    protected function getPages($parentPage)
-    {
-        $tree = GeneralUtility::makeInstance(\TYPO3\CMS\Backend\Tree\Pagetree\DataProvider::class);
-        $pages = $tree->getNodes($parentPage);
-        foreach ($pages as $page) {
-            $this->initializePageAndSubpages($page);
-        }
-
-        return $pages;
-    }
-
-    /**
-     * TODO: Refactor to first gather all page ids, then fetch all information, and add all information?
-     * We have to choose: One query, or one iteration?
-     */
-    protected function initializePageAndSubpages($page)
-    {
-        // Fetch additional information
-        $this->addLanguageOverlay($page);
-        if ($page->getChildNodes()) {
-            foreach ($page->getChildNodes() as $subPage) {
-                $this->initializePageAndSubpages($subPage);
-            }
-        }
-    }
-
-    protected function addLanguageOverlay($page)
-    {
-        $additionalInformation = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-            '*',
-            'pages_language_overlay',
-            'pid = ' . (int) $page->getId()
-        );
-        $record = $page->getRecord();
-        $record['languageOverlay'] = $additionalInformation;
-        $page->setRecord($record);
-    }
-
-    protected function getLanguages()
-    {
-        return $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-            '*',
-            'sys_language',
-            '1=1'
-        );
-    }
+    // TODO: Add method to localize pages
 }
