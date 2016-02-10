d4dcfd235a6ddbde0f97c2cefec59bfee72f69dd
Daniel Siepmann <d.siepmann@web-vision.de>
2016-02-10 12:50:53 +0100

    [BUGFIX] Make svg file names upper case
    * To prevent issues on case sensitive file systems
    
    Related: DSI-68

---
+++ Classes/ViewHelpers/Format/UppercaseViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+namespace WebVision\WvTranslation\ViewHelpers\Format;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Will make the given string fully upper case.
+ *
+ * Will work on wrapped content / inline. So no further arguments are
+ * available.
+ *
+ * @author Daniel Siepmann <d.siepmann@web-vision.de>
+ */
+class UppercaseViewHelper extends AbstractViewHelper
+{
+    /**
+     * Render method to process
+     *
+     * @return void
+     */
+    public function render() {
+        return GeneralUtility::strtoupper(
+            $this->renderChildren()
+        );
+    }
+}
